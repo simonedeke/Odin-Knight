f5af7a6d84111db78bdb21d06680f537c3e9045d
github_simonedeke <simonedeke@gmail.com>
2023-05-16 11:53:13 -0500

    Complete, not commented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {knightMoves} from "./knightMoves.js";
 
+knightMoves([7,7],[0,0]);
 
-knightMoves([0,0],[3,3]);
+knightMoves([0,0],[7,7]);
 
-
+knightMoves([1,6],[7,0]);
--- src/knightMoves.js
+++ src/knightMoves.js
@@ -1,6 +1,50 @@
 export const knightMoves = (start, end) => {
-    console.log('knightmoves');
-    console.log(start);
-    console.log(end);
-    console.log('new');
-}
+    function _isOnBoard(x, y) {
+        return (x >= 0 && x < 8 && y >= 0 && y < 8);
+    }
+    function knightNode(coords) {
+        let x = coords[0];
+        let y = coords[1];
+        let parent = null;
+        return {parent, coords, x, y}
+    }
+    function returnPath(path){
+        let fullPath = [];
+        while(path != null){
+            fullPath.push([path.x, path.y]);
+            path = path.parent;
+        }
+        console.log('The shortest path is:');
+        console.log(fullPath);
+        return fullPath;
+    }
+    const _moveX = [1, 2, -1, -2, 2, 1, -1, -2];
+    const _moveY = [2, 1, 2, 1, -1, -2, -2, -1];
+    let startNode = knightNode(start);
+    let endNode = knightNode(end);
+    
+    let que = [];
+    que.push(startNode);
+    while(que.length > 0){
+        let currentNode = que.shift();
+        if(currentNode.x == end[0] && currentNode.y == end[1]){
+            return returnPath(currentNode);
+        }
+        for(let i = 0; i < 8; i++){
+            let nextX = currentNode.x + _moveX[i];
+            let nextY = currentNode.y + _moveY[i];
+            if(_isOnBoard(nextX,nextY)){
+                let childNode = knightNode([nextX,nextY]);
+                childNode.parent = currentNode;
+                if(nextX == endNode.x && nextY == endNode.y){
+                    i = 10;
+                    return returnPath(childNode);
+                }
+                else {
+                    que.push(childNode);
+                }
+            }            
+        }
+    }
+    return "not possible";
+};
